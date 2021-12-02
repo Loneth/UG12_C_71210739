571b40ac476718513ca69fbbe3b72beeaaeaef58
dae <notloneth@gmail.com>
2021-12-03 00:05:39 +0700

    Mengganti nama variable input

--- 4_C_71210739.py
+++ 4_C_71210739.py
@@ -1,11 +1,11 @@
 # Odilio Ganesha Nugroho - 71210739
 # Unguided 12 Grup C Soal 4
 
-a = int(input("Input : "))
+n = int(input("Input : "))
 
-for x in range(a):
-    for y in range(a):
-        if y == 0 or x == (a - 1) or x == y:
+for x in range(n):
+    for y in range(n):
+        if y == 0 or x == (n - 1) or x == y:
             print("*", end=" ")
         else:
             print(end="  ")
